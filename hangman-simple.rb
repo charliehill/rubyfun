af7171954228dd9fd50d67b08f857cb2a348d6e5
Charlie Hill <charliehill@charlies-mbp.cambridge.ibm.com>
2014-06-26 13:42:04 -0400

    Stripped down hangman without error checking

---
+++ hangman-simple.rb
@@ -0,0 +1,142 @@
+#!/usr/bin/ruby
+
+# Complete hangman game
+# Open a Terminal window 80 x 25 characters in size
+# Hiding the screen, User A enters a word and hands the computer to User B. 
+# User B tries to guess the word with the usual rules. 
+# This version models the game by manipulating strings. 
+# Charlie Hill
+
+# ==========================================================
+# C O N S T A N T S   &   G L O B A L   V A R I A B L E S
+
+# The alphabet 
+Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# Left margin when drawing the current game state 
+Margin = "                  "
+
+# ==========================================================
+# M E T H O D S
+
+# Method to draw a text-only hangman matching the current progress
+def draw_hangman(wrong_letters_count, hangman_string)
+	s = ""
+	case wrong_letters_count
+	when 0
+		hangman_string[1] = " |    | "
+	when 1
+		hangman_string[2] = " |    O "
+	when 2
+		hangman_string[3] = " |    | "
+	when 3
+		hangman_string[3] = " |   -| "
+	when 4
+		hangman_string[3] = " |   -|-"
+	when 5
+		hangman_string[4] = " |   /  "
+	when 6
+		hangman_string[4] = " |   / \\"
+	end
+	hangman_string.each { |str| puts Margin + str }
+end
+
+# Method to draw the current state of the hangman, the letters used, and the puzzle word
+def draw_state(letters_found, letters_tried, wrong_letters_count, hangman_string)
+	12.times { puts }
+	# Draw the hangman
+	draw_hangman(wrong_letters_count, hangman_string)
+	2.times { puts }
+	# Draw the letters used
+	if letters_tried.length >0 
+		print Margin + "Letters tried: " + letters_tried.upcase
+	end
+	2.times { puts }
+	# Print the letters found in the puzzle word
+	print Margin
+	letters_found.each_char { |c| print c.upcase + " " }
+	4.times { puts }
+end
+
+# ==========================================================
+# M A I N   P R O G R A M 
+
+# Welcome message
+25.times { puts } 
+puts "Welcome to Hangman"
+puts
+
+# Get the word then clear the screen for the second user
+print "What is the word? "
+word = gets.chomp.upcase
+25.times { puts } 
+
+# Create a string matching the input word, replacing every letter with an underscore 
+letters_found = String.new(word)
+for i in 0...word.length 
+	letters_found[i] = "_"
+end
+
+# Make am empty string to hold the letters tried
+letters_tried = ""
+
+# Create counter for wrong letters
+wrong_letters_count = 0
+
+# Create the initial state of the hangman as an arrage of strings
+hangman_string = Array.new(6)
+hangman_string[0] = " ______"
+hangman_string[1] = " |     "
+hangman_string[2] = " |     "
+hangman_string[3] = " |     "
+hangman_string[4] = " |     "
+hangman_string[5] = "_|_    "
+
+# Draw the initial state
+draw_state(letters_found, letters_tried, wrong_letters_count, hangman_string)
+
+# Main input loop: 
+# Get a letter from the user, redraw the hangman, and break when done or dead 
+while wrong_letters_count < 6 do 
+	# Get a letter from the user
+	guess = gets.chomp.upcase
+	letters_tried += guess
+	# If the guess is present in the string, update the letters_found
+	if word.include? guess
+		for i in 0...word.length
+			if word[i] == guess
+				letters_found[i] = guess
+			end
+		end
+	else
+		wrong_letters_count += 1
+	end
+	# Test whether the puzzle is done (if letters_found includes "_" then it's not) 
+	if letters_found.index("_") == nil
+		complete = true
+	else
+		complete = false
+	end	
+	# If it is complete then it's time to exit the main loop, and wrap up
+	if complete 
+		break
+	end
+
+	# Draw the new state of the hangman, the letters used, and the puzzle string
+	draw_state(letters_found, letters_tried, wrong_letters_count, hangman_string)
+end
+
+# Redraw the final state
+draw_state(letters_found, letters_tried, wrong_letters_count, hangman_string)
+
+# If the puzzle if complete then print a victory message
+if complete 
+	print Margin + "You did it!"
+# Otherwise, print a crushing message of defeat
+else
+	print Margin + "Bummer. Your man is hanged!\n"
+	print Margin + "Correct answer: " + word
+end
+
+2.times { puts } 
+gets
+puts
